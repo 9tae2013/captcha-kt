a927cfe7c8425a1e36f85ad665e60f782296a914
piya_l <piya.tae@gmail.com>
2019-10-30 22:47:34 +0700

    use xor to select operand func

--- src/main/kotlin/com/example/demo/captcha.kt
+++ src/main/kotlin/com/example/demo/captcha.kt
@@ -1,10 +1,9 @@
 package com.example.demo
 
-val numOperand = fun(i: Int): String = i.toString()
-val strOperand = fun(i: Int): String = arrayOf("Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine")[i]
+val numberOperand: (Int) -> String = { it.toString() }
+val stringOperand: (Int) -> String = { arrayOf("Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine")[it] }
+val operand = fun(pattern: Int, position: Int, number: Int) = listOf(numberOperand, stringOperand)[position xor pattern](number)
 val operator = fun(i: Int): String = arrayOf("+", "-", "*")[i]
 
-fun captcha(pattern: Int, left: Int, oper: Int, right: Int) = arrayOf(
-        "${numOperand(left)} ${operator(oper)} ${strOperand(right)}",
-        "${strOperand(left)} ${operator(oper)} ${numOperand(right)}"
-)[pattern]
+fun captcha(pattern: Int, left: Int, operator: Int, right: Int) =
+        listOf(operand(pattern, 0, left), operator(operator), operand(pattern, 1, right)).joinToString(" ")
